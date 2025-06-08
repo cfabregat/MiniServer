885b4e3afe9cd767484d605a48173bc2d2578149
Cristian Mauro FABREGAT <cmfabregat@gmail.com>
2025-06-08 19:39:30 -0300

    gzip

--- HttpResponse.cs
+++ HttpResponse.cs
@@ -1,26 +1,59 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 static class HttpResponse
 {
-    public static string Ok(string content)
+    public static string Ok(string content, bool enableCompression = true)
     {
-        return BuildResponse(200, "OK", content);
+        return BuildResponse(200, "OK", content, enableCompression);
     }
 
-    public static string NotFound(string content)
+    public static string NotFound(string content, bool enableCompression = true)
     {
-        return BuildResponse(404, "Not Found", content);
+        return BuildResponse(404, "Not Found", content, enableCompression);
     }
 
-    private static string BuildResponse(int statusCode, string statusText, string content)
+    private static string BuildResponse(int statusCode, string statusText, string content, bool enableCompression)
     {
         var sb = new StringBuilder();
+        byte[] contentBytes = Encoding.UTF8.GetBytes(content);
+        bool isCompressed = false;
+        string contentEncoding = string.Empty;
+
+        if (enableCompression)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress))
+                {
+                    gzipStream.Write(contentBytes, 0, contentBytes.Length);
+                }
+                contentBytes = memoryStream.ToArray();
+                isCompressed = true;
+                contentEncoding = "Content-Encoding: gzip\r\n";
+            }
+        }
+
         sb.AppendLine($"HTTP/1.1 {statusCode} {statusText}");
         sb.AppendLine("Content-Type: text/html; charset=utf-8");
-        sb.AppendLine($"Content-Length: {Encoding.UTF8.GetByteCount(content)}");
+        sb.AppendLine(contentEncoding);
+        sb.AppendLine($"Content-Length: {contentBytes.Length}");
         sb.AppendLine("Connection: close");
         sb.AppendLine();
-        sb.Append(content);
+
+        // Convert bytes to string for text-based HTTP response
+        if (isCompressed)
+        {
+            // For compressed content, encode to Base64 to safely include in string response
+            sb.Append(Convert.ToBase64String(contentBytes));
+        }
+        else
+        {
+            sb.Append(content);
+        }
+
         return sb.ToString();
     }
 }
